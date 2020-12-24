15ce984789f879d14a4897d38d64f04af0afc349
Thomas Navarro <thomas.navarro@outlook.com>
2020-12-24 13:56:45 +0100

    fix form

--- components/Form.vue
+++ components/Form.vue
@@ -6,7 +6,7 @@
             Merci de remplir ce formulaire ci-dessous nous permettant de récolter ton score de ouf !
         </div>
 
-        <form name="jeu-concours" action="/pages/merci" method="post" data-netlify="true" data-netlify-honeypot="bot-field" enctype="multipart/form-data" class="max-w-3xl m-auto flex flex-col space-y-6">
+        <form name="jeu-concours" action="/merci" method="post" data-netlify="true" data-netlify-honeypot="bot-field" enctype="multipart/form-data" class="max-w-3xl m-auto flex flex-col space-y-6">
 
             <input type="hidden" name="form-name" value="jeu-concours" />
 
