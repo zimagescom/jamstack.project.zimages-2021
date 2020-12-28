ebfcff4264a6abc742dfab6801b358a82046efbc
Thomas Navarro <thomas.navarro@outlook.com>
2020-12-28 15:00:53 +0100

    update form

--- pages/merci.vue
+++ pages/merci.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="mt-16 flex flex-col bg-purple-100 text-black rounded-xl p-6 max-w-3xl m-auto shadow-xl">
+    <div class="mt-20 flex flex-col bg-purple-100 text-black rounded-xl p-6 max-w-3xl m-auto shadow-xl">
         <h1 sr-only="2021 pas de Game Over, juste du Fun et du Bonheur ! • Jeu concours 2021 by Zimages"></h1>
         <div class="text-center text-4xl text-pink-600 font-medium">Whaaaooouuu, mais quel score !!  </div>
         <div class="text-center text-2xl text-gray-600">Merci de votre participation !</div>
