8a7bb6bf9677acd54bc7b6b01dc6369a533b81ee
Thomas Navarro <thomas.navarro@outlook.com>
2020-12-29 14:49:48 +0100

    fix forms

--- components/Form.vue
+++ components/Form.vue
@@ -6,7 +6,9 @@
                 class="bg-pink-100 text-pink-400 rounded-full px-3 py-1">1ère borne arcade ZIMAGES</span> ! <br />
             Merci de remplir ce formulaire ci-dessous nous permettant de récolter ton score de ouf !
         </div>
-        <form action="https://kwes.io/api/foreign/forms/YjDMJ9rwGHU2UOCBFVei" class="kwes-form max-w-3xl m-auto flex flex-col space-y-6">
+        <form name="voeux-zimages-2021" method="post" action="/merci" data-netlify="true"
+            data-netlify-honeypot="bot-field" class="kwes-form max-w-3xl m-auto flex flex-col space-y-6">
+            <input type="hidden" name="form-name" value="voeux-zimages-2021" />
             <div>
                 <label class="text-sm ml-2" for="name">Prénom et Nom *</label>
                 <input type="text" name="name" id="name"
@@ -22,10 +24,10 @@
                 <input type="number" min="0" name="record" id="record"
                     class="shadow border-none form-input block rounded-full focus:shadow-outline-pink focus:border-pink" />
             </div>
-            <div>
+            <!-- <div>
                 <label class="text-sm ml-2" for="score">Capture d'écran de mon super giga méga score</label>
                 <input ref="file" type="file" name="score" id="score" class="shadow border-none form-input block rounded-full focus:shadow-outline-pink focus:border-pink" />
-            </div>
+            </div> -->
             <div>
                 <input type="checkbox" name="accept_terms" id="accept_terms" required
                     class="shadow border-none form-checkbox text-xl mr-2 bg-white focus:shadow-outline-pink focus:border-pink" />
@@ -33,7 +35,8 @@
                     données pour me contacter *</label>
             </div>
             <div>
-                <button type="submit" class="rounded-full mt-4 px-10 py-3 uppercase bg-pink font-bold text-xl text-white shadow-md transition duration-100 hover:bg-pink-600 hover:shadow-lg focus:outline-none focus:shadow-outline-pink">Envoyer</button>
+                <button type="submit"
+                    class="rounded-full mt-4 px-10 py-3 uppercase bg-pink font-bold text-xl text-white shadow-md transition duration-100 hover:bg-pink-600 hover:shadow-lg focus:outline-none focus:shadow-outline-pink">Envoyer</button>
             </div>
             <div class="text-xs text-pink-400">*Champs obligatoires</div>
         </form>
