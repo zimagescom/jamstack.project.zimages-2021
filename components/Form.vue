7b6f719d4949d21d76074696c0b1e1d2ab9919c7
Thomas Navarro <thomas.navarro@outlook.com>
2020-12-29 11:04:35 +0100

    update form

--- components/Form.vue
+++ components/Form.vue
@@ -6,7 +6,7 @@
                 class="bg-pink-100 text-pink-400 rounded-full px-3 py-1">1ère borne arcade ZIMAGES</span> ! <br />
             Merci de remplir ce formulaire ci-dessous nous permettant de récolter ton score de ouf !
         </div>
-        <form name="voeux-jeu-concours" action="/merci/" method="POST" netlify class="max-w-3xl m-auto flex flex-col space-y-6">
+        <form name="voeux-jeu-concours" action="/" method="POST" netlify class="max-w-3xl m-auto flex flex-col space-y-6">
             <!-- <input type="hidden" name="form-name" value="voeux-jeu-concours" /> -->
             <div>
                 <label class="text-sm ml-2" for="name">Prénom et Nom *</label>
